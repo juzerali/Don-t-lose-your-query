a815e2d5bf0f96a8502d48297cb3b830556dc655
Juzer Ali <er.juzerali@gmail.com>
2013-02-19 21:47:17 +0530

    add /devadmin/ to whitelist

--- Firefox-Addon/resources/dont-lose-your-query/lib/main.js
+++ Firefox-Addon/resources/dont-lose-your-query/lib/main.js
@@ -9,7 +9,7 @@ var pageMod = require("page-mod");
 var data = require("self").data;
 
 pageMod.PageMod({
-    include: [/.*phpminiadmin.*/, /.*phpmyadmin.*/],
+    include: [/.*phpminiadmin.*/, /.*phpmyadmin.*/, /.*devadmin.*/],
     contentScriptWhen: 'ready',
     contentScriptFile: [data.url('jquery-1.7.2.min.js'),data.url('jquery-ui-1.8.20.custom.min.js'),data.url('bootstrap.min.js'),data.url('querysaver.js')]
 
